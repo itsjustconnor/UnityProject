01406e094d9f0634b2749388e2719bd063db76cc
itsjustconnor <36472448+itsjustconnor@users.noreply.github.com>
2018-03-07 10:49:29 +0000

    adding more code such as moving right, using functions

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class player : MonoBehaviour {
 
 	public string toPrintClass = "Hello world as class variable";
+	public float speed =5;
 
 	// Use this for initialization
 	void Start () {
 		string toPrint = "Hello World as a variable";
 		Debug.Log (toPrintClass);
+		string returned1 = TestFunction ("TestFunction Variable message", 1);
+		string returned2 = TestFunction ("TestFunction Variable message", 2);
+		Debug.Log ("returned1 = " + returned1);
+			Debug.Log("returned2 = " + returned2);
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D> ();
+		ourRigidBody.velocity = Vector2.right * speed;
+	}
+
+	string TestFunction(string message, int count) {
+		Debug.Log (message + " " + count);
+
+		return "Returned String";
 	}
 }
