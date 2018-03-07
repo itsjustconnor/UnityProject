1f563486c0dd3d04a37c1c862b2cc78ca18a4a94
itsjustconnor <36472448+itsjustconnor@users.noreply.github.com>
2018-03-07 10:09:47 +0000

    added new sprites , audio and added a logging player script

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public string toPrintClass = "Hello world as class variable";
+
+	// Use this for initialization
+	void Start () {
+		string toPrint = "Hello World as a variable";
+		Debug.Log (toPrintClass);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
