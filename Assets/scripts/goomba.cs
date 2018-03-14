ba391e53984fc7485d320763bba0e5ab0a88e85a
itsjustconnor <36472448+itsjustconnor@users.noreply.github.com>
2018-03-14 10:52:29 +0000

    added damageing effects to mario and added a goomba

---
+++ Assets/scripts/goomba.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goomba : MonoBehaviour {
+
+	//public variables(variable in editor)
+	public float damage = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//called  when trigger collides with anouther collider
+	void OnTriggerStay2D(Collider2D other)
+	{
+		//attempting to get the player script from the colider
+		player playerscript = other.GetComponent<player> ();
+
+		//if the player script does collide
+		if (playerscript != null) {
+			// calling the damage function on the script then applying the damage
+			playerscript.Damage (damage);
+
+			Debug.Log ("goomba dealt damage to player = " + damage);
+		}//end if (playerscript != null)
+	} //end ontriggerenter2d()
+
+
+
+
+}
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-	public string toPrintClass = "Hello world as class variable";
+
 	public float speed =5;
+	public float health = 100;
+	public float invulnerableDuration = 1;
+
+	private float invulnerableEndTime = 0;
 
 	// Use this for initialization
 	void Start () {
-		string toPrint = "Hello World as a variable";
-		Debug.Log (toPrintClass);
-		string returned1 = TestFunction ("TestFunction Variable message", 1);
-		string returned2 = TestFunction ("TestFunction Variable message", 2);
-		Debug.Log ("returned1 = " + returned1);
-			Debug.Log("returned2 = " + returned2);
+		
 
-
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D> ();
 		ourRigidBody.velocity = Vector2.right * speed;
 	}
+	
+	//reducing health by the damage passed in
+	public void Damage(float damageToDeal)
+	{
+		if (Time.time >= invulnerableEndTime) {
+			health = health - damageToDeal;
+			//TODO: Handle death
 
-	string TestFunction(string message, int count) {
-		Debug.Log (message + " " + count);
+			//set us as invulnerable for a set duration
+			invulnerableEndTime = Time.time + invulnerableDuration;
 
-		return "Returned String";
-	}
+			//Log the result of the function 
+			Debug.Log ("damage Was dealt");
+			Debug.Log ("damagetodeal = " + damageToDeal);
+			Debug.Log ("damagetodeal = " + health);
+		}
+			
+	}//end damage ()
 }
